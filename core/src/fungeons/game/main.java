54902f3d3fe42c7b0a49121c1555aa39e9c72b67
GaurabAryal <gaurab.aryal7@gmail.com>
2015-02-12 10:07:55 -0500

    Updated to table layout

--- core/src/fungeons/game/main.java
+++ core/src/fungeons/game/main.java
@@ -3,8 +3,12 @@ package fungeons.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -33,67 +37,61 @@ import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class main extends ApplicationAdapter {
-    Object[] listEntries = {"This is a list entry1", "And another one1", "The meaning of life1", "Is hard to come by1",
-            "This is a list entry2", "And another one2", "The meaning of life2", "Is hard to come by2", "This is a list entry3",
-            "And another one3", "The meaning of life3", "Is hard to come by3", "This is a list entry4", "And another one4",
-            "The meaning of life4", "Is hard to come by4", "This is a list entry5", "And another one5", "The meaning of life5",
-            "Is hard to come by5"};
-
     Skin skin;
     Stage stage;
-    Label fpsLabel;
+    Music LoginSong;
+    SpriteBatch batch;
+    Texture img;
 
     @Override
     public void create () {
-        skin = new Skin(Gdx.files.internal("uiskin.json"));
-        // stage = new Stage(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), false, new PolygonSpriteBatch());
-        stage = new Stage(new ScreenViewport());
-        Gdx.input.setInputProcessor(stage);
+        batch = new SpriteBatch();
+        img = new Texture("bgimg2.jpeg");
+        LoginSong = Gdx.audio.newMusic(Gdx.files.internal("login.mp3"));
         int height = Gdx.graphics.getHeight();
         int width = Gdx.graphics.getWidth();
-        // Group.debug = true;
-
-
-        TextField textfield = new TextField("", skin);
-        textfield.setMessageText("ex.John101");
-        textfield.setAlignment(Align.left);
-        CheckBox idRem = new CheckBox("Remember ID", skin);
-        idRem.setChecked(true);
-        CheckBox pwRem = new CheckBox("Remember ID", skin);
-        fpsLabel = new Label("fps:", skin);
-        // configures an example of a TextField in password mode.
+        skin = new Skin(Gdx.files.internal("uiskin.json"));
+        stage = new Stage(new ScreenViewport());
         final Label passwordLabel = new Label("Password: ", skin);
         final Label userLabel = new Label("Username: ", skin);
         final Label someSpace = new Label("         ", skin);
+        final TextField passwordTextField = new TextField("", skin);
+        Gdx.input.setInputProcessor(stage);
+        TextField textfield = new TextField("", skin);
+        textfield.setMessageText("ex.John101");
+        textfield.setAlignment(Align.left);
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
+        textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
+        textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
+        textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
+        final TextButton button = new TextButton("Login!", skin);
+        skin.add("default", textButtonStyle);
+        CheckBox idRem = new CheckBox("Remember ID", skin);
+        idRem.setChecked(true);
+        CheckBox pwRem = new CheckBox("Remember ID", skin);
         userLabel.setFontScaleX(1.5f);
         userLabel.setFontScaleY(1.5f);
         passwordLabel.setFontScaleX(1.5f);
         passwordLabel.setFontScaleY(1.5f);
         someSpace.setFontScaleX(1.5f);
         someSpace.setFontScaleY(1.5f);
-        final TextField passwordTextField = new TextField("", skin);
         passwordTextField.setMessageText("*****");
         passwordTextField.setPasswordCharacter('*');
         passwordTextField.setPasswordMode(true);
-
-        // window.debug();
-        Window window = new Window("Login", skin);
-
-        window.setPosition((width/5), height);
-        window.row();
-        window.add(userLabel).colspan(1);
-        window.add(textfield).minWidth(width/2).minHeight(height/8).expandX().fillX().colspan(1);
-        window.row();
-        window.add(passwordLabel).colspan(1);
-        window.add(passwordTextField).minWidth(width/2).minHeight(height/8).expandX().fillX().colspan(1);
-        window.row();
-
-        window.row();
-        window.pack();
-        window.setMovable(false);
-        window.setWidth((float)width);
-        // stage.addActor(new Button("Behind Window", skin));
-        stage.addActor(window);
+        Table table = new Table();
+        table.add(userLabel).padBottom(10).padRight(25);
+        table.add(textfield).width(300).padBottom(10);
+        table.row();
+        table.add(passwordLabel).padBottom(10).padRight(25);
+        table.add(passwordTextField).width(300).padBottom(10);
+        table.row();
+        table.add(button).padBottom(10).padRight(25);
+        table.setFillParent(true);
+        stage.addActor(table);
+        LoginSong.setVolume(1f);
+        LoginSong.setLooping(true);
+        LoginSong.play();
         textfield.setTextFieldListener(new TextFieldListener() {
             public void keyTyped (TextField textField, char key) {
                 if (key == '\n') textField.getOnscreenKeyboard().show(false);
@@ -103,9 +101,10 @@ public class main extends ApplicationAdapter {
 
     @Override
     public void render () {
-        Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        batch.begin();
+        batch.draw(img, 0, 0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        batch.end();
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         stage.draw();
     }
