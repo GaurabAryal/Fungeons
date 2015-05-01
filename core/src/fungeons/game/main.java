5579081ab2b6b73356c213599e6aaeacc3bc81e7
RadutRares <JonSnowofNightsWatch1@gmail.com>
2015-05-01 09:55:51 -0400

    listview

--- core/src/fungeons/game/main.java
+++ core/src/fungeons/game/main.java
@@ -9,6 +9,8 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -36,7 +38,6 @@ public class main extends Game {
     ScrollPane scrollPane;
     TextureAtlas atlas;
 
-
     @Override
     public void create () {
         //final Parse parse = new Parse();
@@ -53,79 +54,38 @@ public class main extends Game {
         LoginSong.setVolume(1f);
         LoginSong.setLooping(true);
         LoginSong.play();
-        OrthographicCamera camera = new OrthographicCamera(nWidth, nHeight);
-        camera.position.set(nWidth/2,nHeight/2, 0);
-        camera.update();
-
-        //atlas = new TextureAtlas("ui/atlas.pack");
-        //skin = new Skin(Gdx.files.internal(ui/menuSkin.json),skin);
-
-        table = new Table(skin);
-        table.debug();
 
         list = new List(skin);
-        list.setItems(new Object[]{"Johnathan"+"                     "+"Fun City",
-                "Justnathan"+"                     "+"Buns Town",
-                "SomeGuy5"+"                     "+"Buns Town",
-                "Player Name"+"                     "+"Map Name",
-                "just kidding",
-                "I'm sorry"});
+        String arsNames[] = new String[50];
+        for(int i=0;i<50;i++){
+            arsNames[i]="J'son"+i;
+        }
+        list.setItems(arsNames);
 
+        list.getSelected();
         scrollPane = new ScrollPane(list,skin);
+        scrollPane.setFillParent(true);
 
-        //table.add("Players Active");
-        //table.add(list);
-        //table.setPosition(450f,450f);
-        //list.setPosition(450f,450f);
-
-        scrollPane.setPosition(nWidth/2, nHeight/2);
-        scrollPane.setWidth(400f);
-        scrollPane.setHeight(200f);
+        scrollPane.addListener(new EventListener() {
+            @Override
+            public boolean handle(Event event) {
+                final DialogB infoB = new DialogB(list.getSelected().toString(),skin);
+                //infoB.SetMap("I've been Defeated");// I spent 20 minutes relearning to
+                // making a class for this when it's already a prebuilt function ...
+                infoB.text("Map Placeholder");
+                infoB.show(stage);
+                /*infoB.addCaptureListener(new EventListener() {
+                    @Override
+                    public boolean handle(Event event) {
+                        infoB.remove();
+                        return false;
+                    }
+                });*/
+               return false;
+            }
+        });
         stage.addActor(scrollPane);
 
-        //final ExitDialog exitDialog = new ExitDialog("Error", skin);
-        //final Label passwordLabel = new Label("Password: ", skin);
-        //final Label userLabel = new Label("Username: ", skin);
-        //final Label someSpace = new Label("         ", skin);
-        //final TextField txtPassword = new TextField("", skin);
-        //final TextButton button = new TextButton("Login!", skin);
-        //final TextField txtUsername = new TextField("", skin);
-        //txtUsername.setMessageText("ex.John101");
-        //txtUsername.setAlignment(Align.left);
-        //TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
-        //textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
-        //textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
-        //textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
-        //textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
-
-        //skin.add("default", textButtonStyle);
-        /*userLabel.setFontScaleX(1.5f);
-        userLabel.setFontScaleY(1.5f);
-
-        passwordLabel.setFontScaleX(1.5f);
-        passwordLabel.setFontScaleY(1.5f);
-
-        someSpace.setFontScaleX(1.5f);
-        someSpace.setFontScaleY(1.5f);
-
-        txtPassword.setMessageText("*****");
-        txtPassword.setPasswordCharacter('*');
-        txtPassword.setPasswordMode(true);
-
-        table.add(userLabel).padBottom(10).padRight(25);
-        table.add(txtUsername).width(300).padBottom(10);
-        table.row();
-        table.add(passwordLabel).padBottom(10).padRight(25);
-        table.add(txtPassword).width(300).padBottom(10);
-        table.row();
-        table.add(button).padBottom(10).padRight(25);
-        table.setFillParent(true);
-        stage.addActor(table);
-        //
-        Label nameLabel = new Label("Name:", skin);
-        TextField nameText = new TextField("", skin);
-        Label addressLabel = new Label("Address:", skin);
-        TextField addressText = new TextField("", skin);*/
         //exitDialog.show(stage);//This makes Dialog Box pop up, it automatically centers itself
         //txtUsername.setTextFieldListener(new TextFieldListener() {
         /*    public void keyTyped (TextField textField, char key) {
@@ -166,27 +126,31 @@ public class main extends Game {
         skin.dispose();
     }
 
-    /*public static class ExitDialog extends Dialog {
-        public ExitDialog(String title, Skin skin,String windowStyle){
+    public static class DialogB extends Dialog{
+        //String sMap;
+        public DialogB(String title, Skin skin,String windowStyle){
             super(title, skin, windowStyle);
         }
-        public ExitDialog(String title, Skin skin){
+        public DialogB(String title, Skin skin){
             super(title, skin);
 
         }
 
-        public ExitDialog(String title,WindowStyle windowStyle){
+        public DialogB(String title,WindowStyle windowStyle, String sMap){
             super(title, windowStyle);
         }
+        /*public void SetMap(String _sMap){
+            _sMap = sMap;
+        }*/
         {
             setScale(1.5f,1.5f);
-            text("An unexpected error has occurred");
+            //text(sMap+"WTF");
             button("OK");
 
         }
         @Override
         protected void result(Object object){
-           // System.out.println("result");
+
         }
-    }*/
+    }
 }
