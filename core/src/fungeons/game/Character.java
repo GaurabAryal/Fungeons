9bd34e027c5396e73e9d8ad002763ac4de38a018
TheCompassMan <ben_southern@hotmail.com>
2015-05-07 10:50:14 -0400

    Added character as a box2D object plus analog stick to move the character

---
+++ core/src/fungeons/game/Character.java
@@ -0,0 +1,126 @@
+package fungeons.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+
+/**
+ * Created by Ben on 2015-04-28.
+ */
+public class Character extends Sprite {
+    int nCharX=128, nCharY=128, nDeltaY, nOldX=128, nOldY=128, nDir=2, Columns=6, Rows=6, nImgHeight, nImgWidth;
+    //Dir 1 is left, Dir 2 is right
+    int nCharVX, nCharVY;
+    Animation WalkR,WalkL,StandR,StandL,JumpR,JumpL, CurAnim;
+    TextureAtlas.AtlasRegion CharSheet;
+    TextureAtlas Atlas;
+    Boolean bCanJump=true;
+
+    BodyDef CharDef;
+    Body CharBody;
+    FixtureDef CharFixDef;
+    PolygonShape CharBox;
+    Play play;
+
+    public void create(){
+
+        play = new Play();
+        Atlas= new TextureAtlas(Gdx.files.internal("Fungeons_2.pack"));
+        CharSheet=Atlas.findRegion("Fungeon Char 64 W");
+        nImgHeight=CharSheet.getRegionHeight()/Rows;
+        nImgWidth=CharSheet.getRegionWidth()/Columns;
+        TextureRegion[][] Character= CharSheet.split(nImgWidth, nImgHeight);
+
+        WalkR=new Animation(0.075f,Character[0]);
+        WalkL=new Animation(0.075f,Character[1]);
+        StandR=new Animation(0.2f,Character[2]);
+        StandL=new Animation(0.2f,Character[3]);
+        JumpR=new Animation(0.075f,Character[4]);
+        JumpL=new Animation(0.075f,Character[5]);
+        CurAnim=StandR;
+
+        CharDef=new BodyDef();
+        CharBox= new PolygonShape();
+        CharFixDef=new FixtureDef();
+
+
+        CharBox.setAsBox(16f,32f);
+        CharDef.position.set(128, 128);
+        CharFixDef.shape=CharBox;
+        CharDef.type= BodyDef.BodyType.DynamicBody;
+
+        CharBody=play.world.createBody(CharDef);
+        CharFixDef.density=1f;
+        CharFixDef.restitution=0f;
+        CharFixDef.friction=0;
+
+        CharBody.createFixture(CharFixDef);
+    }
+    public void setVars(int VX, int VY, int X, int Y, int Dir, Boolean CanJump){
+        nDir=Dir;
+        bCanJump=CanJump;
+        nCharX=X;
+        nCharY=Y;
+        nCharVX=VX;
+        nCharVY=VY;
+        if(nCharVX<0){
+            nDir=1;
+        }
+        else if(nCharVX>0){
+            nDir=2;
+        }
+        if(nCharVY!=0){
+            bCanJump=false;
+        }
+        if(nDir==1){//left
+            if(bCanJump==true){
+                if(nCharVX!=0){
+                    CurAnim=WalkL;
+                }
+                else if(nCharVX==0){
+                    CurAnim=StandL;
+                }
+            }
+            else{
+                CurAnim=JumpL;
+            }
+        }
+        if(nDir==2){//right
+            if(bCanJump==true){
+                if(nCharVX!=0){
+                    CurAnim=WalkR;
+                }
+                else if(nCharVX==0){
+                    CurAnim=StandR;
+                }
+            }
+            else{
+                CurAnim=JumpR;
+            }
+        }
+    }
+    public Character(){
+
+    }
+    public int getCharX(){
+        return(nCharX);
+    }
+    public int getCharY(){
+        return(nCharY);
+    }
+    public boolean getCanJump(){
+        return(bCanJump);
+    }
+    public Animation getCharAnim(){
+        return(CurAnim);
+    }
+    public Body getCharBody(){
+        return(CharBody);
+    }
+}
--- core/src/fungeons/game/Play.java
+++ core/src/fungeons/game/Play.java
@@ -5,7 +5,10 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
@@ -21,27 +24,35 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Touchpad;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
-import com.badlogic.gdx.utils.Array;
 
 public class Play extends Game {
     OrthographicCamera camera;
-    Vector2 gravity=new Vector2(0,-9.8f), CurMove=new Vector2(0,0);
-    BodyDef MapDef, CharDef;
-    Body MapBody, CharBody;
-    FixtureDef MapFixDef, CharFixDef;
-    PolygonShape MapBox, CharBox;
-    ScreenControl screenControl;
-    World world;
-    int nScreenHeight, nScreenWidth,CharX =64,CharY=64, nTileHeight, nTileWidth;
-    double VX = 0, VY = 0;
-    Box2DDebugRenderer b2Renderer;
+    Vector2 gravity=new Vector2(0,-9.8f);
+    World world = new World(gravity, false);
 
-    Array<Body> arWorldBodies = new Array<Body>();
+    BodyDef MapDef;
+    Body MapBody, CharBody;
+    FixtureDef MapFixDef;
+    PolygonShape MapBox;
+
+    Box2DDebugRenderer b2Renderer;
 
     TiledMap Map;
     TiledMapTileLayer MapCol, MapAlt;
     TmxMapLoader MapLoader;
     OrthogonalTiledMapRenderer MapRenderer;
+
+    int nScreenHeight, nScreenWidth, nTileHeight, nTileWidth;
+    float Time=0;
+    TextureRegion Frame;
+
+    Animation CurAnim;
+    int nCharX =128,nCharY=128;
+    int nCharVX, nCharVY;
+    int nDir=2;
+
+    TextureAtlas Atlas;
+    TextureRegion[][] MoveBG1, MoveKnob1;
 
     SpriteBatch batch;
     Touchpad touchpadMove;
@@ -51,8 +62,41 @@ public class Play extends Game {
     Stage stage;
     Boolean bCanJump=false, bLight=true;
 
+    Character character = new Character();
+    ScreenControl screenControl;
+
     @Override
     public void create() {
+        Atlas= new TextureAtlas(Gdx.files.internal("Fungeons_2.pack"));
+
+        character.create();
+
+        stage=new Stage();
+        TextureAtlas.AtlasRegion Region;
+        int RegionHeight, RegionWidth;
+        Region = Atlas.findRegion("TouchPad BackGround",-1);
+        RegionHeight=Region.getRegionHeight();
+        RegionWidth=Region.getRegionWidth();
+        MoveBG1 = Region.split(RegionWidth,RegionHeight);
+        Region = Atlas.findRegion("Touchpad Knob",-1);
+        RegionHeight=Region.getRegionHeight();
+        RegionWidth=Region.getRegionWidth();
+        MoveKnob1 = Region.split(RegionWidth,RegionHeight);
+
+        touchpadMoveSkin = new Skin();//making a touchpad which is kinda like an analog stick
+        touchpadMoveSkin.add("MoveKnob", MoveKnob1[0][0]);
+        touchpadMoveSkin.add("MoveBackground", MoveBG1[0][0]);
+        touchMoveKnob = touchpadMoveSkin.getDrawable("MoveKnob");
+        touchMoveBackground = touchpadMoveSkin.getDrawable("MoveBackground");
+        touchpadMoveStyle = new Touchpad.TouchpadStyle();
+        touchpadMoveStyle.knob = touchMoveKnob;
+        touchpadMoveStyle.background = touchMoveBackground;
+        touchpadMove = new Touchpad(0, touchpadMoveStyle);
+        touchpadMove.setSize(200, 200);
+        touchpadMove.setPosition(0, 0);
+
+        stage.addActor(touchpadMove);
+
         MapLoader=new TmxMapLoader();
         Map=MapLoader.load("BunsTown.tmx");
         MapCol= (TiledMapTileLayer) Map.getLayers().get(0);
@@ -65,9 +109,8 @@ public class Play extends Game {
         camera=new OrthographicCamera();
         camera.viewportHeight=nScreenHeight*2;
         camera.viewportWidth=nScreenWidth*2;
-        camera.position.set(500,500,0);
+        camera.position.set(nCharX,nCharY,0);
 
-        world = new World(gravity, false);
         b2Renderer=new Box2DDebugRenderer();
 
         MapDef= new BodyDef();
@@ -76,42 +119,81 @@ public class Play extends Game {
 
         nTileHeight=(int)MapCol.getTileHeight();
         nTileWidth=(int)MapCol.getTileWidth();
-
+        CharBody=character.getCharBody();
         batch= new SpriteBatch();
-        System.out.println(MapCol.getWidth());
-        for(int i =0; i< MapCol.getWidth(); i++){
-            for(int j=0; j<MapCol.getHeight(); j++){
+
+        for(int i =0; i< MapCol.getWidth(); i++){//makes a 2D grid of box2D rectangles overtop the tiled map
+            for(int j=0; j<MapCol.getHeight(); j++){ // Awesome right?
                 if(MapCol.getCell(i,j).getTile().getProperties().containsKey("Hit")) {
                     MapDef= new BodyDef();
                     MapFixDef= new FixtureDef();
                     MapBox= new PolygonShape();
                     MapDef.position.set(i*nTileWidth+nTileWidth/2, j*nTileHeight+ nTileHeight/2);//sets the box to proper coordinates to correlate to the tiled map
                     MapBody = world.createBody(MapDef);
-                    MapBox.setAsBox(nTileWidth/2, nTileHeight/2);
+                    MapBox.setAsBox((nTileWidth+20)/2, nTileHeight/2);
                     MapBody.createFixture(MapBox, 0f);
-                    arWorldBodies.add(MapBody);
                 }
             }
-
         }
-
-
+        CharBody=world.createBody(character.CharDef);//grabs the character definition from character file
+        CharBody.createFixture(character.CharFixDef);//grabs the character's fixture definition from character file
     }
+
     @Override
     public void render(){
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        if(screenControl.nScreen==3) {
+            Gdx.input.setInputProcessor(stage);
+        }
+
+        CurAnim=character.getCharAnim();
+        Time += Gdx.graphics.getDeltaTime();           // #15
+        Frame = CurAnim.getKeyFrame(Time, true);
+
         camera.update();
-        b2Renderer.render(world, camera.combined);
+
+        nCharX=character.getCharX();
+        nCharY=character.getCharY();
+
+        if(touchpadMove.getKnobPercentX()>=0.50){
+            nCharVX=150;
+        }
+        else if(touchpadMove.getKnobPercentX()<=-0.50){
+            nCharVX=-150;
+        }
+        else{
+            nCharVX=0;
+        }
+        CharBody.setLinearVelocity(nCharVX,CharBody.getLinearVelocity().y);
 
         batch.setProjectionMatrix(camera.combined);
         MapRenderer.setView(camera);
         MapRenderer.render();
-        batch.begin();
 
+        batch.begin();
+        batch.draw(Frame, nCharX - 32, nCharY - 32);
         batch.end();
 
+        b2Renderer.render(world, camera.combined);
         world.step(1/60f, 6, 2);
+        nCharX=(int)CharBody.getPosition().x;
+        nCharY=(int)CharBody.getPosition().y;
+        nCharVY=(int)CharBody.getLinearVelocity().y;
+        nCharVX=(int)CharBody.getLinearVelocity().x;
+
+        if(nCharVX<0){
+            nDir=1;
+        }
+        if(nCharVX>0){
+            nDir=2;
+        }
+        bCanJump=MapCol.getCell((int) ((nCharX/2)/nTileWidth), (int) ((nCharY-nTileHeight) / nTileHeight))//Collide Down
+                .getTile().getProperties().containsKey("Hit");
+
+        character.setVars(nCharVX, nCharVY, nCharX, nCharY, nDir, bCanJump);
+        stage.draw();
     }
     @Override
     public void dispose(){
@@ -120,4 +202,5 @@ public class Play extends Game {
         screenControl = screenControl_;
     }
 }
+
 
